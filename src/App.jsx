253cc34ac1d535ec6bbbbff3fe7a86816a21fc05
deesii <donnagph@hotmail.com>
2024-04-12 15:00:29 +0100

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import MyButton from './components/button'
+import NewSection from './components/projects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      
+      <div className = "card-section">
+      <NewSection>
+
+      </NewSection>
+      </div>
+
       <div>
 
       </div>
-      <h1>Recipe Book</h1>
-      <div className="card">
+      <h1>About Me</h1>
+      <div className="card-section">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -22,9 +30,11 @@ function App() {
       <p className="read-the-docs">
         
       </p>
-      <p>
+      <div className='card-section'>
+  
         <MyButton></MyButton>
-      </p>
+      
+      </div>
     </>
   )
 }
---
+++ src/components/projects/index.jsx
@@ -0,0 +1,10 @@
+export default function NewSection(){
+    return(
+        
+        <div>
+            <h3>New Section</h3>
+        
+            <button>Find out more</button>
+        </div>
+    )
+}
