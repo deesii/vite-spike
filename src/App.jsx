c5118a11ea16571e830ff5911cca478ad8348f39
deesii <donnagph@hotmail.com>
2024-04-17 12:40:46 +0100

    added contact links and images

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,14 @@ function App() {
   return (
     <>
       <h1>deesii</h1>
-      
-        
+      <div className = "contact-row">
+      <a className = "contact-link" href="https://www.linkedin.com/in/donna-cheng-deesii/" rel="nofollow noreferrer">
+        <img src="https://i.stack.imgur.com/gVE0j.png" alt="LinkedIn Image" />
+      </a>
+      <a className = "contact-link" href="https://github.com/deesii" rel="nofollow noreferrer">
+        <img src="https://i.stack.imgur.com/tskMh.png" alt="GitHub Image" />
+      </a>
+      </div>  
         <ul>
             <li><Link to="/tictactoe">TicTacToe</Link></li>
             <li><Link to="/counthowhigh">Counts</Link></li>
@@ -22,7 +28,6 @@ function App() {
           <Route path="/counthowhigh" element={<CountComponent />} />
           <Route path="/goodbyes" element={<Who />} />
         </Routes>
-    
     </>
   )
 }
